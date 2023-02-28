12efcfa47efbd3279c6b84ccbded4503c80e4fa5
applica2022 <jappdev2028@gmail.com>
2023-02-28 16:38:17 -0500

    first commit

---
+++ docs/config.js
@@ -0,0 +1,25 @@
+/* eslint-disable quotes */
+(function() {
+  zuix.store('config', {
+    "title": "News Blog",
+    "baseUrl": "/",
+    "language": "en",
+    "resourcePath": "/app/",
+    "libraryPath": {
+      "@lib": "/lib/1.1/",
+      "@hgui": "https://genielabs.github.io/homegenie-web-ui/app/",
+      "@cdnjs": "https://cdnjs.cloudflare.com/ajax/libs/"
+    },
+    "zuixjs.github.io": {
+      "baseUrl": "/news-chicago/",
+      "resourcePath": "/news-chicago/app/",
+      "libraryPath": {
+        "@lib": "/news-chicago/lib/1.1/",
+        "@hgui": "https://genielabs.github.io/homegenie-web-ui/app/",
+        "@cdnjs": "https://cdnjs.cloudflare.com/ajax/libs/"
+      }
+    },
+    "siteMapUrl": "https://montanojd.github.io/news-chicago",
+    "environment": "default"
+  });
+})();
---
+++ source/content/local/local1/index.md
@@ -0,0 +1,16 @@
+---
+layout: basic.liquid
+tags: local
+group: local
+
+
+coverImage: https://picsum.photos/seed/local!local1/960/600
+coverSmall: https://picsum.photos/seed/local!local1/640/400
+coverPreview: https://picsum.photos/seed/local!local1/320/200
+title: Local1
+description: Page description
+author:
+pubDate: 2023-02-28 04:28:27
+keywords:
+- Local1
+---
